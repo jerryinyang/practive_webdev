4053d2c72714ff74fd7679dc6ba86cc469826eb6
Jerry Inyang <jerprog0@gmail.com>
2024-06-11 20:47:56 +0100

    commencing `chart page` development

--- script.js
+++
@@ -1,6 +0,0 @@
-import { showMessage } from './utils.js';
-
-document.getElementById('clickMe').addEventListener('click', function() {
-    showMessage('Button Clicked!');
-});
-
--- utils.js
+++
@@ -1,3 +0,0 @@
-export function showMessage(message) {
-    document.getElementById('message').innerText = message;
-}
