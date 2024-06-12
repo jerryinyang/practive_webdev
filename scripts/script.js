3b06f2e1252b137f6adbe50617fef8da7c22934a
Jerry Inyang <jerprog0@gmail.com>
2024-06-12 03:26:15 +0100

    development on header on the landing page in progress

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+placeholderElement1 = document.querySelector(".placeholder-1")
+placeholderElement2 = document.querySelector(".placeholder-2")
+
+placeholderElement1.innerHTML = "Placeholder 1"
+placeholderElement2.innerHTML = "Placeholder 2"
